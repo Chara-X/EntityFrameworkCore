6f450402264d281ebd5bdbc8e45c33afccaf9a57
Chara-X <52699657+Chara-X@users.noreply.github.com>
2021-01-09 17:43:56 +0800

    Add files via upload

---
+++ Middlewares/Concrete/Collections/DbSet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using EntityFrameworkCore.Collections.Infrastructure;
+using EntityFrameworkCore.Expressions.Concrete;
+using EntityFrameworkCore.Middlewares.Tools;
+
+namespace EntityFrameworkCore.Collections
+{
+    public class DbSet<T> : IEnumerable<T>
+    {
+        private readonly DbSetLogger logger;
+
+        public DbProxy Proxy => logger.Proxy;
+
+        public Expression Expression { get; set; }
+
+        internal DbSet(DbSetLogger logger)
+        {
+            this.logger = logger;
+            Expression = new UnionExpression(typeof(T));
+        }
+
+        internal DbSet(DbSetLogger logger, Expression expression)
+        {
+            this.logger = logger;
+            Expression = expression;
+        }
+
+        public void Add(T entity) => logger.LogAdd(entity);
+
+        public void Remove(T entity) => logger.LogRemove(entity);
+
+        public void Update(T entity) => logger.LogUpdate(entity);
+
+        public IEnumerator<T> GetEnumerator() => Proxy.Reader<T>(Expression).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Middlewares/Concrete/Collections/Infrastructure/DbSetLogger.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EntityFrameworkCore.Middlewares.Tools;
+using EntityFrameworkCore.ORMapping;
+
+namespace EntityFrameworkCore.Collections.Infrastructure
+{
+    public class DbSetLogger
+    {
+        private readonly Dictionary<string, EntityLog> logs = new Dictionary<string, EntityLog>();
+
+        public DbProxy Proxy { get; set; }
+
+        public DbSetLogger(DbProxy proxy) => Proxy = proxy;
+
+        public void LogAdd(object entity)
+        {
+            var primaryKeyValue = PortableType.Create(entity.GetType()).GetPrimaryKeyValue(entity);
+            if (logs.ContainsKey(primaryKeyValue))
+                logs.Remove(primaryKeyValue);
+            logs.Add(primaryKeyValue, new EntityLog(entity, EntityState.Added));
+        }
+
+        public void LogRemove(object entity)
+        {
+            var primaryKeyValue = PortableType.Create(entity.GetType()).GetPrimaryKeyValue(entity);
+            if (logs.ContainsKey(primaryKeyValue))
+                logs.Remove(primaryKeyValue);
+            logs.Add(primaryKeyValue, new EntityLog(entity, EntityState.Removed));
+        }
+
+        public void LogUpdate(object entity)
+        {
+            var primaryKeyValue = PortableType.Create(entity.GetType()).GetPrimaryKeyValue(entity);
+            if (logs.ContainsKey(primaryKeyValue))
+            {
+                var log = logs[primaryKeyValue];
+                switch (log.State)
+                {
+                    case EntityState.Added:
+                    case EntityState.Updated: log.Entity = entity; return;
+                    case EntityState.Removed: return;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            logs.Add(primaryKeyValue, new EntityLog(entity, EntityState.Updated));
+        }
+
+        public int Submit()
+        {
+            return logs.Values.Sum(log => log.State switch
+            {
+                EntityState.Added => Proxy.Insert(log.Entity),
+                EntityState.Updated => Proxy.Update(log.Entity),
+                EntityState.Removed => Proxy.Delete(log.Entity),
+                _ => throw new ArgumentOutOfRangeException()
+            });
+        }
+
+        public class EntityLog
+        {
+            public object Entity { get; set; }
+
+            public EntityState State { get; set; }
+
+            public EntityLog(object entity, EntityState state)
+            {
+                Entity = entity;
+                State = state;
+            }
+        }
+
+        public enum EntityState
+        {
+            Added,
+            Updated,
+            Removed
+        }
+    }
+}
